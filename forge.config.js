48ebd72a2f4767c920e6992ede3b08b47679939e
IvanMurash <murashkin1@yandex.ru>
2023-03-11 22:13:05 +0400

    add cert

--- forge.config.js
+++ forge.config.js
@@ -2,6 +2,12 @@ module.exports = {
   makers: [
     {
       name: '@rabbitholesyndrome/electron-forge-maker-portable',
+      config: {
+        win: {
+          certificateFile: 'CheckTeammatesLoL.pfx',
+          certificatePassword: process.env.CERT_PASSWORD,
+        },
+      },
     },
   ],
 };
